eb0642a2c6ad7edaa5d7c9175f25e70306020166
Hugh Woodfall <hugh.woodfall@au.ey.com>
2020-05-21 18:14:59 +1000

    Using datetime.today module to filter only patches released today

--- parsertongue.py
+++ parsertongue.py
@@ -1,31 +1,39 @@
 ## Import libraries
 import feedparser
 from tabulate import tabulate
+import time
+from datetime import date
+# Todays date
+#today = date.today
+# Test date
+today = "2020-05-14"
+#print(today)
 
 ## Variable declarations
 # Set the RSS URL
 url= "https://alas.aws.amazon.com/alas.rss"
 # Create empty arrays for the XML elements
-date_list = []
 title_list = []
 link_list = []
 # Define headers for table output
-headers  = ["Date","Title","Link"]
+headers  = ["Title","Link"]
 
 ## RSS Parsing
 feed = feedparser.parse(url)
 # For each item in the feed, put the relevant element into array
 for item in feed.entries:
-    date = "(%d/%02d/%02d)" % (item.published_parsed.tm_year, \
-        item.published_parsed.tm_mon, \
-        item.published_parsed.tm_mday)
-    date_list.append(date)
-    title_list.append(item.title)
-    link_list.append(item.link)
+    date = "%d-%02d-%02d" % (item.published_parsed.tm_year, \
+            item.published_parsed.tm_mon, \
+            item.published_parsed.tm_mday)
+    if date == today:
+        title_list.append(item.title)
+        link_list.append(item.link)
 
 # Create table variable with zipped element lists
-table = zip(date_list, title_list, link_list)
+table = zip(title_list, link_list)
 # Print table using tabulate library
+print(len(title_list), "patch(es) came out on", today,":")
 print(tabulate(table, headers=headers))
 
-print("Number of total RSS items:", len(feed.entries))
+# Debugging
+print("Total RSS items:", len(feed.entries))
