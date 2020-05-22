419028546dd5f0c9ebee0676f75f99ba2bca1190
Hugh Woodfall <hugh.woodfall@au.ey.com>
2020-05-22 15:57:01 +1000

    Add email function using gmail SMTP host

---
+++ send_email.py
@@ -0,0 +1,20 @@
+import smtplib
+import config
+
+def send_email(subject, msg):
+    try:
+        server = smtplib.SMTP('smtp.gmail.com:587')
+        server.ehlo()
+        server.starttls()
+        server.login(config.SENDER_EMAIL, config.PASSWORD)
+        message = 'Subject: {}\n\n{}'.format(subject, msg)
+        server.sendmail(config.SENDER_EMAIL,config.RECEIVER_EMAIL, message)
+        server.quit()
+        print("Success: Email sent.")
+    except:
+        print("Email failed to send.")
+
+subject = "Test subject"
+msg = "Hello World!"
+
+send_email(subject, msg)
